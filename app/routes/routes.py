c73093e8167831e18b83a32abc7f1727bd561e73
Diederik Mathijs <diederik.mathijs@gmail.com>
2020-10-05 20:35:05 +0200

    Styling, basic dashboard

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -11,19 +11,7 @@ bp = Blueprint('routes', __name__)
 @bp.route("/")
 @protected_route
 def index():
-    # if not linkedin.authorized:
-    #     return redirect(url_for("linkedin.login"))
-    # resp = linkedin.get("me")
-    # assert resp.ok
-    # data = resp.json()
-    # name = "{first} {last}".format(
-    #     first=preferred_locale_value(data["firstName"]),
-    #     last=preferred_locale_value(data["lastName"]),
-    # )
-    # resp2 = linkedin.get("emailAddress?q=members&projection=(elements*(handle~))")
-    # print(resp2.json())
-    # return "You are {name} on LinkedIn".format(name=name)
-    return "OK"
+    return render_template('index.html')
 
 @bp.route('/login')
 def login():
